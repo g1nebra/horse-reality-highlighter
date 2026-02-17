6d34352ebc2fdbb41883faa0fb581a39211b85f3
g1nebra <110259662+g1nebra@users.noreply.github.com>
2026-02-17 20:20:17 +0100

    Update content.js

--- content.js
+++ content.js
@@ -2,9 +2,13 @@
 const TARGET_COATS = [
     // W8 icelandic
   "mxsi6xn7n5cjjmqo7einfzvzkoaiae332l6i7agcutzadgusqdmjgg5vwvat7bmfr4jdg22jrn6wq.png",
+  
   // GENERIC COAT FOR DEBUG
-  "c6qajgteybe3xcib3fotvjmjnt77dlnl63avcir34ya4fjnz6hopamksa2fecufl74x5ufzyfrira.png"
-
+  // "c6qajgteybe3xcib3fotvjmjnt77dlnl63avcir34ya4fjnz6hopamksa2fecufl74x5ufzyfrira.png",
+  // "77q6gyxcbnf6njmfhjwfv5mox5y3clguvp2iensztpngntxz7qax242gqdefdidteievzmjgwrofy.png",
+  // "un67ffxfpfcbzg57h4xd4x6h6f2ea3y3z65fqiliuxczuppysks53sgf3vt32zkuaf6s5tn2pficy.png",
+  // "p6bpdv2ombez7ofqoidd5x5c467e4z2umpmuyx4rj7ivlja3p5bpmhzmnvtsj5p7vv4wgiu55wdns.png",
+  // "usbgkqfvzvgfvhwfhvbu326ljrpcalsgn7q744kuxqb6fpzyjjo42czu3eah64j7uw44p6fxaptla.png"
 ];
 
 const TARGET_BREED = "Icelandic Horse";
@@ -22,9 +26,14 @@ function highlightHorses() {
     const coatMatches = TARGET_COATS.some(coatId => imgSrc.includes(coatId));
 
     if (breedMatches && coatMatches) {
-      block.style.border = "5px solid #2ecc71";
-      block.style.backgroundColor = "rgba(46, 204, 113, 0.1)";
-      block.style.borderRadius = "10px";
+      block.style.backgroundColor = " #ff0000";
+
+      const buyButton = block.querySelector('.buy_fhorse');
+
+      if (buyButton) {
+        buyButton.style.height = "55px";
+        buyButton.style.fontWeight = "bold";
+      }
     }
   });
 }
