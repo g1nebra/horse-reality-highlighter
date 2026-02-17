9d7a933d485d835f138666c090161e40a2ca7e69
g1nebra <110259662+g1nebra@users.noreply.github.com>
2026-02-17 21:08:18 +0100

    Update content.js

--- content.js
+++ content.js
@@ -4,14 +4,10 @@ const TARGET_COATS = [
   "mxsi6xn7n5cjjmqo7einfzvzkoaiae332l6i7agcutzadgusqdmjgg5vwvat7bmfr4jdg22jrn6wq.png",
   "rtnjzvjfojfzreregldc24eks3d65dwaoyoah3vj5tdkbxufn47ocwaqsaq2u4vfglqbgrhkgm7dg.png",
   "3f4bmehwjnhx5egc55pjer7lckwx7bg6iwhsnng5i4b7yjx234ycuhfkb3nzpg6fk4eah3bpn6gqy.png",
-  "46l2wiq7e5be3n246dexcfxhow37c3psgnq4dtj4tkxkpw5qvbugabw2wl34ja2olvof5qtvxyfoe.png"
+  "46l2wiq7e5be3n246dexcfxhow37c3psgnq4dtj4tkxkpw5qvbugabw2wl34ja2olvof5qtvxyfoe.png",
+  "2riz763rtzenddwk75gkambidzn5k5b5zrhkdwlbqtjulxsm4prifte4rm5a7asx2tqr4k3auvc64.png",
+  "zusimh4e4jg2nmwwdh3xxaxbwryr6avw7fsofnaemrtjeq2mbjhqqetqv6oq3gqk2dgjund4ltmtc.png"
 
-  // GENERIC COAT FOR DEBUG
-  // "c6qajgteybe3xcib3fotvjmjnt77dlnl63avcir34ya4fjnz6hopamksa2fecufl74x5ufzyfrira.png",
-  // "77q6gyxcbnf6njmfhjwfv5mox5y3clguvp2iensztpngntxz7qax242gqdefdidteievzmjgwrofy.png",
-  // "un67ffxfpfcbzg57h4xd4x6h6f2ea3y3z65fqiliuxczuppysks53sgf3vt32zkuaf6s5tn2pficy.png",
-  // "p6bpdv2ombez7ofqoidd5x5c467e4z2umpmuyx4rj7ivlja3p5bpmhzmnvtsj5p7vv4wgiu55wdns.png",
-  // "usbgkqfvzvgfvhwfhvbu326ljrpcalsgn7q744kuxqb6fpzyjjo42czu3eah64j7uw44p6fxaptla.png"
 ];
 
 const TARGET_BREED = "Icelandic Horse";
